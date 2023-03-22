0584799bb70f858b36eac5174cddb6d3502bf73f
Karl <westerholm.karl@gmail.com>
2023-03-22 20:03:33 +0100

    Starting with some semantic HTML and React component setup, also added meta tags to index.html

--- final-capstone-project/src/App.js
+++ final-capstone-project/src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ final-capstone-project/src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, HStack } from "@chakra-ui/react";
+
+function Nav() {
+    <>
+        <img src="Logo.svg" />
+        <ul>
+            <li>
+                <a>Home</a>
+            </li>
+            <li>
+                <a>About</a>
+            </li>
+        </ul>
+    </>
+};
