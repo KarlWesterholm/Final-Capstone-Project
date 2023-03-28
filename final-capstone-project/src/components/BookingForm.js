59a6ca5b477577a63d41104152b9f206fcb529c3
Karl <westerholm.karl@gmail.com>
2023-03-28 23:12:44 +0200

    Added booking page and initialized booking form. Form still needs to be edited and changed to be functional and work with state.

--- final-capstone-project/src/components/BookingForm.js
+++ final-capstone-project/src/components/BookingForm.js
@@ -1,10 +1,36 @@
-import React from "react";
+import { React, useState, useRef } from "react";
 
 const BookingForm = () => {
+    const dateRef = useRef(null);
+    const timeRef = useRef(null);
+    const guestRef = useRef(null);
+    const occasionRef = useRef(null)
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("17")
+    const [guests, setGuests] = useState("2");
+    const [occasion, setOccasion] = useState("None");
     return (
         <>
             <form style={{display: "grid", maxWidth: "200px", gap: "20px"}}>
-                <label for="res-date">Choose date</label>
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" onChange={input => setDate(input)}/>
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time" onChange={input => setTime(input)}>
+                    <option>17:00</option>
+                    <option>18:00</option>
+                    <option>19:00</option>
+                    <option>20:00</option>
+                    <option>21:00</option>
+                    <option>22:00</option>
+                </select>
+                <label htmlFor="guests">Number of guests</label>
+                <input type="number" placeholder="2" min="2" max="10" id="guests" onChange={input => setGuests(input)}/>
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion" onChange={input => setOccasion(input)}>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+                <input type="submit" value="Make your reservation"/>
             </form>
         </>
     );
---
+++ final-capstone-project/src/components/BookingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BookingForm from "./BookingForm.js"
+
+const BookingPage = () => {
+    return (
+        <>
+            <h1>Table Reservation</h1>
+            <BookingForm />
+        </>
+    );
+};
+
+export default BookingPage;
